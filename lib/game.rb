ac3b845cb5e0efdc596e258e540726eab56c0be9
James <James.Steel@me.com>
2016-12-01 11:55:11 +0000

    4 tests passing for placing moves on the board

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,31 @@
 class Game
-  
+
+  attr_reader :row_one, :row_two, :row_three, :turn
+
+  def initialize
+   @row_one =   ['-', '-', '-']
+   @row_two =   ['-', '-', '-']
+   @row_three = ['-', '-', '-']
+   @turn = [:X, :O]
+  end
+
+  def play(num, column)
+   row_num = num_to_row(num)
+   row_num[column -1] = @turn.first.to_s
+   switch_player
+  end
+
+
+  private
+
+  def switch_player
+   @turn.reverse!
+  end
+
+  def num_to_row(num)
+    hash = { 1 => @row_one,
+             2 => @row_two,
+             3 => @row_three}
+    hash[num]
+  end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,5 +1,32 @@
 require './lib/game'
 
 describe Game do
-  
+
+  let(:game) { described_class.new}
+
+  describe 'new game' do
+    it 'starts with a blank board' do
+      expect(game.row_one).not_to include('X', 'O')
+      expect(game.row_two).not_to include('X', 'O')
+      expect(game.row_three).not_to include('X', 'O')
+    end
+  end
+
+  describe '#play' do
+    it 'changes the element in the array to current turns symbol' do
+      game.play(1, 1)
+      expect(game.row_one[0]).to eq 'X'
+    end
+
+    it 'swaps the payer over at the end of the turn' do
+      game.play(1, 1)
+      expect(game.turn.first.to_s).to eq 'O'
+    end
+
+    it 'changes over to put Os symbol on the board' do
+      game.play(1, 1)
+      game.play(1, 2)
+      expect(game.row_one[1]).to eq 'O'
+    end
+  end
 end
